3094a47ec02af9d9cd4414b7bde2414ab067ad28
harshit-python <hsaxena1998@gmail.com>
2024-01-18 12:53:21 +0530

    adding program to calculate person age

---
+++ calculate person age.py
@@ -0,0 +1,22 @@
+from datetime import date
+
+class Person:
+    def __init__(self, name, country, date_of_birth):
+        self.name = name
+        self.country = country
+        self.date_of_birth = date_of_birth
+
+
+    def calculate_age(self):
+        today = date.today()
+        age = today.year - self.date_of_birth.year
+        if today < date(today.year, self.date_of_birth.month, self.date_of_birth.day):
+            age -= 1
+        return age
+
+person1 = Person("Harsh Saxena", "India", date(1998, 5, 31))
+print("Person 1:")
+print("Name:", person1.name)
+print("Country:", person1.country)
+print("Date of Birth:", person1.date_of_birth)
+print("Ag:", person1.calculate_age())
