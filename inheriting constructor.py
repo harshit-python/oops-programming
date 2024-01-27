3585ab75d8193619d1dd33eb6f1ceeae9909a356
harsh <harsh.saxena@thoughts2binary.com>
2024-01-27 13:08:11 +0530

    inheriting private members from parent class

--- inheriting constructor.py
+++ inheriting constructor.py
@@ -3,7 +3,7 @@ class Phone:
     def __init__(self, price, brand, camera):
         print("Inside phone constructor")
         self.price = price
-        self.brand = brand
+        self.__brand = brand
         self.camera = camera
 
 
@@ -13,6 +13,11 @@ class Smartphone(Phone):
 
 s = Smartphone(20000, "Apple", 13)
 # this object will inerit constructor from parent class since it does not have its default constructor
-print(s.brand)
 print(s.price)
 print(s.camera)
+
+# this will throw error since we are inheriting private member from parent class
+try:
+    print(s.__brand)
+except Exception as e:
+    print(e)
