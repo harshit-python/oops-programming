17e073b499ef755ba7c362262e5ad5056f39601c
harsh <harsh.saxena@thoughts2binary.com>
2024-01-27 12:59:58 +0530

    basic inheritance

---
+++ inheritance.py
@@ -0,0 +1,25 @@
+# parent class
+class User:
+
+    def login(self):
+        print("Login")
+
+    def register(self):
+        print("Register")
+
+
+# child class student inheriting User class
+class Student(User):
+
+    def enroll(self):
+        print("Enroll")
+
+    def review(self):
+        print("Review")
+
+
+stu1 = Student()
+stu1.enroll()
+stu1.review()
+stu1.login()
+stu1.register()
